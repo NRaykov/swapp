f159cc6e8b4395474019fc86a8554d78d9762319
NRaykov <nediotenev@gmail.com>
2019-10-26 19:11:12 +0300

    Import font-family  'SfDistantGalaxy'

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+/* Bootstrap 4.3 */
+import 'bootstrap/dist/css/bootstrap.css';
+
+import './fonts/SfDistantGalaxy-0l3d.ttf'
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
