fad9cdc0c0b16c9ad59bfcce2b89627dbde80788
NRaykov <nediotenev@gmail.com>
2019-11-07 05:21:54 +0200

    Make Header Component

--- src/App.js
+++ src/App.js
@@ -7,10 +7,8 @@ import { createHttpLink } from 'apollo-link-http';
 import authLink from './client/auth';
 import {typeDefs} from './client/local'
 //Navigation and login
-import {BrowserRouter as Router, Route, Link, Redirect, withRouter} from 'react-router-dom';
+import {BrowserRouter as Router, Link,} from 'react-router-dom';
 import { NavbarBrand, NavbarToggler, Collapse, Nav, NavItem, Navbar} from 'reactstrap';
-import {isAuthenticated, signout} from './guards/auth';
-import Login from './components/login/';
 import LoginButton from './components/login/loginBtn';
 import Container from './components/elements/container/container';
 //Styling
@@ -19,15 +17,9 @@ import './App.css';
 import { ThemeProvider } from 'styled-components/macro';
 import { themes } from './components';
 import ThemeChanger from './components/themeChanger';
-
-//Pages
-import Episodes from "./pages/episodes/episodesList/";
-import Episode from "./pages/episodes/episodeView/";
-import Characters from "./pages/characters/charactersList/";
-import Character from "./pages/characters/characterView/";
-import Starship from "./pages/starships/starshipView";
 import { gql } from 'graphql.macro';
 import MainRouter from "./pages/router/router";
+import {Header} from "./components/header/header";
 
 
 export const RouteComponent = () => {
@@ -52,46 +44,17 @@ export const RouteComponent = () => {
 
     const isAuthed = !!localStorage.getItem('token');
 
-
   cache.writeData({
     data: {
       authenticated: isAuthed,
     },
   });
-
-
-
-  const [isOpen, setIsOpen] = useState(true);
-  const toggle = () => setIsOpen(!isOpen);
-  //****  login Button Component
-
   return (
       <ApolloProvider client={client}>
           <Router>
             <ThemeProvider theme={themes[getTheme]}>
               <Container variant="primary" className="container-main">
-                <header className="header">
-                    <Navbar className="navbar" light expand="md">
-                      <NavbarBrand href="#" className="logo"
-                           onClick={ themeChanger }
-                      >swapp</NavbarBrand>
-                      <NavbarToggler onClick={toggle}/>
-                      <Collapse isOpen={isOpen} navbar>
-                        <Nav className="ml-auto" navbar>
-                          <NavItem>
-                            <Link className="nav-link" to="/">Episodes</Link>
-                          </NavItem>
-                          <NavItem>
-                            <Link className="nav-link" to="/characters">Characters</Link>
-                          </NavItem>
-                          <NavItem>
-                            <LoginButton/>
-                          </NavItem>
-                        </Nav>
-                      </Collapse>
-                    </Navbar>
-                </header>
-
+                  <Header themeChanger={themeChanger}/>
                 <div className="container py-5">
                  <MainRouter themeChanger={themeChanger}/>
                 </div>
---
+++ src/components/header/header.js
@@ -0,0 +1,37 @@
+import {Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem} from "reactstrap";
+import {Link} from "react-router-dom";
+import LoginButton from "../../components/login/loginBtn";
+import React, {useState} from "react";
+
+export const Header = ({themeChanger}) => {
+
+  const [isOpen, setIsOpen] = useState(true);
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+          <>
+          <header className="header">
+            <Navbar className="navbar" light expand="md">
+              <NavbarBrand href="#" className="logo"
+                           onClick={ themeChanger }
+              >swapp</NavbarBrand>
+              <NavbarToggler onClick={toggle}/>
+              <Collapse isOpen={isOpen} navbar>
+                <Nav className="ml-auto" navbar>
+                  <NavItem>
+                    <Link className="nav-link" to="/">Episodes</Link>
+                  </NavItem>
+                  <NavItem>
+                    <Link className="nav-link" to="/characters">Characters</Link>
+                  </NavItem>
+                  <NavItem>
+                    <LoginButton/>
+                  </NavItem>
+                </Nav>
+              </Collapse>
+            </Navbar>
+          </header>
+            </>
+  )
+
+};
